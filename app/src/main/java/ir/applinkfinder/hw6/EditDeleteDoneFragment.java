134abfecb9d68e8c117f2ebe830701f230ccf3a5
Alirezaput <alirezaput@yahoo.com>
2018-12-31 23:25:16 +0330

    add toolbar object and a text view in it, and set the font and size of textview(toolbar)

--- app/src/main/java/ir/applinkfinder/hw6/EditDeleteDoneFragment.java
+++ app/src/main/java/ir/applinkfinder/hw6/EditDeleteDoneFragment.java
@@ -1,16 +1,23 @@
 package ir.applinkfinder.hw6;
 
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -79,6 +86,25 @@ public class EditDeleteDoneFragment extends Fragment {
             title = getArguments().getString(ARG_TITLE);
             detail = getArguments().getString(ARG_DETAIL);
         }
+
+        // ------------- Toolbar -----------
+        ActionBar myActionBar = ((AppCompatActivity)getActivity()).getSupportActionBar();
+
+        RelativeLayout myLayout = new RelativeLayout(getActivity());
+        TextView textview = new TextView(getActivity());
+        textview.setText(R.string.actionbar_editdeletedone_title);
+        textview.setTextColor(Color.WHITE);
+        RelativeLayout.LayoutParams textviewDetails =
+                new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT,RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+        textview.setLayoutParams(textviewDetails);
+        textview.setGravity(Gravity.RIGHT);
+        textview.setTextSize(25);
+        textview.setTypeface(Typeface.createFromAsset(getActivity().getAssets(), "fonts/BNazanin_0.ttf"));
+
+        myActionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        myActionBar.setCustomView(textview);
+
     }
 
     @Override
@@ -94,8 +120,6 @@ public class EditDeleteDoneFragment extends Fragment {
 
         mEditTextTitle.setText(title);
         mEditTextDetail.setText(detail);
-
-
 
         final FragmentManager fragmentManager = getFragmentManager();
         final MyDialogFragment myDialogFragment = new MyDialogFragment();
