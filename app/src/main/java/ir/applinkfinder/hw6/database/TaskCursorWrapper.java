1f28de2975bdb0b06c281e5df01ff3f6ee1b5df8
Alirezaput <alirezaput@yahoo.com>
2019-01-09 12:33:11 +0330

    ezafe kardan e class CursorWrapper, jahate tarif e cursor va peymayesh ruye database

---
+++ app/src/main/java/ir/applinkfinder/hw6/database/TaskCursorWrapper.java
@@ -0,0 +1,61 @@
+package ir.applinkfinder.hw6.database;
+
+import android.database.Cursor;
+import android.database.CursorWrapper;
+import android.util.Log;
+
+import java.util.Date;
+import java.util.UUID;
+
+import ir.applinkfinder.hw6.model.WorksModel;
+
+public class TaskCursorWrapper extends CursorWrapper {
+
+    // constructor
+    public TaskCursorWrapper(Cursor cursor) {
+        super(cursor);
+    }
+
+    public WorksModel getTask() {
+        UUID uuid = UUID.fromString(getString(getColumnIndex(TaskDbSchema.TaskTable.Cols.UUID)));
+        String title = getString(getColumnIndex(TaskDbSchema.TaskTable.Cols.TITLE));
+        String detail = getString(getColumnIndex(TaskDbSchema.TaskTable.Cols.DETAIL));
+        Date date = new Date(getLong(getColumnIndex(TaskDbSchema.TaskTable.Cols.DATE)));
+        Date hour = new Date(getLong(getColumnIndex(TaskDbSchema.TaskTable.Cols.HOUR)));
+        boolean isDone = getInt(getColumnIndex(TaskDbSchema.TaskTable.Cols.DONE)) != 0;
+
+        WorksModel worksModel = new WorksModel(uuid);
+        worksModel.setTitle(title);
+        worksModel.setDetail(detail);
+        worksModel.setDate(date);
+        worksModel.setHour(hour);
+        worksModel.setDone(isDone);
+
+        return worksModel;
+    }//getTask
+
+    public WorksModel getDoneTask() {
+//        if (getTask().isDone()){
+//        if (getInt(getColumnIndex(TaskDbSchema.TaskTable.Cols.DONE)) != 0){
+//        if (true){
+//            if (TaskDbSchema.TaskTable.Cols.DONE.equals("1")){
+            Log.d("Tag22", "getDoneTask isDone"); //test
+            UUID uuid = UUID.fromString(getString(getColumnIndex(TaskDbSchema.TaskTable.Cols.UUID)));
+            String title = getString(getColumnIndex(TaskDbSchema.TaskTable.Cols.TITLE));
+            String detail = getString(getColumnIndex(TaskDbSchema.TaskTable.Cols.DETAIL));
+            Date date = new Date(getLong(getColumnIndex(TaskDbSchema.TaskTable.Cols.DATE)));
+            Date hour = new Date(getLong(getColumnIndex(TaskDbSchema.TaskTable.Cols.HOUR)));
+//        boolean isDone = getInt(getColumnIndex(TaskDbSchema.TaskTable.Cols.DONE)) != 0;
+boolean isDone = true;
+            WorksModel worksModel = new WorksModel(uuid);
+            worksModel.setTitle(title);
+            worksModel.setDetail(detail);
+            worksModel.setDate(date);
+            worksModel.setHour(hour);
+        worksModel.setDone(isDone);
+            return worksModel;
+//        }
+//        return null;
+    }//getDoneTask
+
+}
