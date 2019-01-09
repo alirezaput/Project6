aa7bdbe56bb278eb4497a4a6cd5a460ece784ab0
Alirezaput <alirezaput@yahoo.com>
2019-01-09 19:20:15 +0330

    shart e if ke sahih nabud va dar commit haye qabli comment shode bud, pak shod

--- app/src/main/java/ir/applinkfinder/hw6/database/TaskCursorWrapper.java
+++ app/src/main/java/ir/applinkfinder/hw6/database/TaskCursorWrapper.java
@@ -35,27 +35,20 @@ public class TaskCursorWrapper extends CursorWrapper {
     }//getTask
 
     public WorksModel getDoneTask() {
-//        if (getTask().isDone()){
-//        if (getInt(getColumnIndex(TaskDbSchema.TaskTable.Cols.DONE)) != 0){
-//        if (true){
-//            if (TaskDbSchema.TaskTable.Cols.DONE.equals("1")){
-            Log.d("Tag22", "getDoneTask isDone"); //test
-            UUID uuid = UUID.fromString(getString(getColumnIndex(TaskDbSchema.TaskTable.Cols.UUID)));
-            String title = getString(getColumnIndex(TaskDbSchema.TaskTable.Cols.TITLE));
-            String detail = getString(getColumnIndex(TaskDbSchema.TaskTable.Cols.DETAIL));
-            Date date = new Date(getLong(getColumnIndex(TaskDbSchema.TaskTable.Cols.DATE)));
-            Date hour = new Date(getLong(getColumnIndex(TaskDbSchema.TaskTable.Cols.HOUR)));
+        Log.d("Tag22", "getDoneTask isDone"); //test
+        UUID uuid = UUID.fromString(getString(getColumnIndex(TaskDbSchema.TaskTable.Cols.UUID)));
+        String title = getString(getColumnIndex(TaskDbSchema.TaskTable.Cols.TITLE));
+        String detail = getString(getColumnIndex(TaskDbSchema.TaskTable.Cols.DETAIL));
+        Date date = new Date(getLong(getColumnIndex(TaskDbSchema.TaskTable.Cols.DATE)));
+        Date hour = new Date(getLong(getColumnIndex(TaskDbSchema.TaskTable.Cols.HOUR)));
 //        boolean isDone = getInt(getColumnIndex(TaskDbSchema.TaskTable.Cols.DONE)) != 0;
-boolean isDone = true;
-            WorksModel worksModel = new WorksModel(uuid);
-            worksModel.setTitle(title);
-            worksModel.setDetail(detail);
-            worksModel.setDate(date);
-            worksModel.setHour(hour);
+        boolean isDone = true;
+        WorksModel worksModel = new WorksModel(uuid);
+        worksModel.setTitle(title);
+        worksModel.setDetail(detail);
+        worksModel.setDate(date);
+        worksModel.setHour(hour);
         worksModel.setDone(isDone);
-            return worksModel;
-//        }
-//        return null;
+        return worksModel;
     }//getDoneTask
-
 }
