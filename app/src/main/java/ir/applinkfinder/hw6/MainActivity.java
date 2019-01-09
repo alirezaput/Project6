00ddba61563e44cfbab860bb08fa25886eed5e58
Alirezaput <alirezaput@yahoo.com>
2019-01-09 19:33:11 +0330

    username e karbar ra dar in safhe daryaft mikonad, hamchenin newIntent baraye call kardane MainActivity tarif shod

--- app/src/main/java/ir/applinkfinder/hw6/MainActivity.java
+++ app/src/main/java/ir/applinkfinder/hw6/MainActivity.java
@@ -1,5 +1,7 @@
 package ir.applinkfinder.hw6;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
@@ -9,11 +11,16 @@ public class MainActivity extends AppCompatActivity {
 
     private ViewPager mViewPager;
     private TabLayout tabLayout;
+    private String username;
+
+    private static final String EXTRA_USERNAME = "ir.applinkfinder.hw6.extra_username";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        username = getIntent().getStringExtra(EXTRA_USERNAME);
 
         mViewPager = findViewById(R.id.viewPager);
 
@@ -28,4 +35,10 @@ public class MainActivity extends AppCompatActivity {
         tabLayout = findViewById(R.id.tabLayout);
         tabLayout.setupWithViewPager(mViewPager);
     }//onCreate
+
+    public static Intent newIntent(Context context, String username){
+        Intent intent = new Intent(context, MainActivity.class);
+        intent.putExtra(EXTRA_USERNAME, username);
+        return intent;
+    }//newIntent
 }
