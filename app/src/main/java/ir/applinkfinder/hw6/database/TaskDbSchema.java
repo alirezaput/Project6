bc1977c4c5f086f889678b3b59070496b60c394d
Alirezaput <alirezaput@yahoo.com>
2019-01-09 19:17:02 +0330

    Schema ye table e jadid (Contacts Table) shamele name table, va name e columns tarif shod

--- app/src/main/java/ir/applinkfinder/hw6/database/TaskDbSchema.java
+++ app/src/main/java/ir/applinkfinder/hw6/database/TaskDbSchema.java
@@ -19,4 +19,15 @@ public class TaskDbSchema {
     }//CrimeTable
 
     //Table 2: Login...
+    public static final class ContactsTable {
+        public static final String NAME = "contacts"; // Table Name
+
+        public static final class Cols {
+            public static final String ID = "id";
+            public static final String NAME = "name";
+            public static final String USERNAME = "username";
+            public static final String PASSWORD = "password";
+            public static final String EMAIL = "email";
+        }//Cols
+    }//ContactsTable
 }
