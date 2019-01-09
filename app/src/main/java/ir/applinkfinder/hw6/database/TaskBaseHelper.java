2e93da671bbe470271d93d81152c76e232b304c7
Alirezaput <alirezaput@yahoo.com>
2019-01-09 12:32:07 +0330

    ezafe kardane class SQLiteOpenHelper va sakhte database dar an, va sakhte tasks Table ba tedade moshakhasi columns

---
+++ app/src/main/java/ir/applinkfinder/hw6/database/TaskBaseHelper.java
@@ -0,0 +1,34 @@
+package ir.applinkfinder.hw6.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class TaskBaseHelper extends SQLiteOpenHelper {
+
+    //constructor
+    public TaskBaseHelper(Context context){
+        super(context, TaskDbSchema.NAME, null, TaskDbSchema.VERSION); //name: esme db
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // db.execSQL(create table myTableName(Col1Name text, Col2Name integer, ...));
+        db.execSQL("create table " + TaskDbSchema.TaskTable.NAME + "(" +
+                "_id integer primary key autoincrement, " +
+                TaskDbSchema.TaskTable.Cols.UUID + ", " +
+                TaskDbSchema.TaskTable.Cols.TITLE + ", " +
+                TaskDbSchema.TaskTable.Cols.DETAIL + ", " +
+                TaskDbSchema.TaskTable.Cols.DATE + ", " +
+                TaskDbSchema.TaskTable.Cols.HOUR + ", " +
+                TaskDbSchema.TaskTable.Cols.DONE +
+                ")"
+        );
+    }
+
+    //alter kardan: ezafe kardane column and .... dar onUpgrade anjam mishe
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
