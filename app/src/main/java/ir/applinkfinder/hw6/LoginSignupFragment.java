659e35eb81bb180435ec21d9af05371dc3f6ad85
Alirezaput <alirezaput@yahoo.com>
2019-01-11 21:01:16 +0330

    pass contact id to MainActivity

--- app/src/main/java/ir/applinkfinder/hw6/LoginSignupFragment.java
+++ app/src/main/java/ir/applinkfinder/hw6/LoginSignupFragment.java
@@ -1,6 +1,8 @@
 package ir.applinkfinder.hw6;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -12,6 +14,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import ir.applinkfinder.hw6.database.TaskBaseHelper;
+import ir.applinkfinder.hw6.database.TaskDbSchema;
 import ir.applinkfinder.hw6.model.WorksRepository;
 
 /**
@@ -41,10 +45,29 @@ public class LoginSignupFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 String password = WorksRepository.getInstance(getActivity()).searchContactPassword(mEditTextUsername.getText().toString());
-
+                int contactId;
                 if (mEditTextPassword.getText().toString().equals(password)){
                     Log.d("mytag", "login");
-                    Intent intent = MainActivity.newIntent(getActivity(), mEditTextUsername.getText().toString());
+
+                    SQLiteDatabase db;
+                    db = new TaskBaseHelper(getActivity()).getReadableDatabase();
+                    String user = mEditTextUsername.getText().toString();
+                    String query = "select * from " + TaskDbSchema.ContactsTable.NAME + " where " +
+                            TaskDbSchema.ContactsTable.NAME + "." +TaskDbSchema.ContactsTable.Cols.USERNAME + " = " + "'" + user + "'";
+                    Cursor cursor = db.rawQuery(query, null);
+                    try {
+                        // cursor ruye db peymayesh mikone
+                        cursor.moveToFirst();
+                        contactId = cursor.getInt(cursor.getColumnIndex(TaskDbSchema.ContactsTable.Cols.CONTACT_ID));
+                    }// try
+                    finally {
+                        cursor.close();
+                    }
+
+//                    ContactIdFinder contactIdFinder = new ContactIdFinder();
+//                    contactId = contactIdFinder.getContactId(mEditTextUsername.getText().toString());
+
+                    Intent intent = MainActivity.newIntent(getActivity(), contactId);
                     startActivity(intent);
                 }
                 else {
