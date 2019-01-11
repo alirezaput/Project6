008509c269a30b7832269ee56626fa26d5d9177e
Alirezaput <alirezaput@yahoo.com>
2019-01-11 20:50:34 +0330

    add contactId getter to this activity

--- app/src/main/java/ir/applinkfinder/hw6/AddItemToListActivity.java
+++ app/src/main/java/ir/applinkfinder/hw6/AddItemToListActivity.java
@@ -6,16 +6,21 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class AddItemToListActivity extends AppCompatActivity {
 
     private static final String EXTRA_TITLE = "ir.applinkfinder.hw6v3.extra_title";
     private static final String EXTRA_DETAIL = "ir.applinkfinder.hw6v3.extra_detail";
+    private static final String EXTRA_CONTACT_ID = "ir.applinkfinder.hw6.extra_contact_id";
 
+    private int contact_Id;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item_to_list);
+
+        getContactIdInAddItem();
 
         // Begin the transaction
         FragmentManager fragmentManager = getSupportFragmentManager();
@@ -27,10 +32,16 @@ public class AddItemToListActivity extends AppCompatActivity {
         }
     }//onCreate
 
+    public int getContactIdInAddItem(){
+        contact_Id = getIntent().getIntExtra(EXTRA_CONTACT_ID, -2);
+        Toast.makeText(this, "AddItemToListActivity Welcome User: " + contact_Id, Toast.LENGTH_SHORT).show();
+        return contact_Id;
+    }
+
     //add
-    public static Intent newIntent(Context context){
+    public static Intent newIntent(Context context, int contactId){
         Intent intent = new Intent(context, AddItemToListActivity.class);
-//        intent.putExtra();
+        intent.putExtra(EXTRA_CONTACT_ID, contactId);
         return intent;
     }//newIntent
 }
