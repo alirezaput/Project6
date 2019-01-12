36856c3eb71fede2a44fbe8d1b7b60fcb5bfbfa2
Alirezaput <alirezaput@yahoo.com>
2019-01-13 01:07:41 +0330

    set Dialog size and title in onResume replace editFragment as a fragment, with editFragment as a dialogFragment

--- app/src/main/java/ir/applinkfinder/hw6/EditDeleteDoneFragment.java
+++ app/src/main/java/ir/applinkfinder/hw6/EditDeleteDoneFragment.java
@@ -4,9 +4,8 @@ import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
@@ -14,10 +13,9 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.List;
 import java.util.UUID;
@@ -25,7 +23,7 @@ import java.util.UUID;
 import ir.applinkfinder.hw6.model.WorksModel;
 import ir.applinkfinder.hw6.model.WorksRepository;
 
-public class EditDeleteDoneFragment extends Fragment {
+public class EditDeleteDoneFragment extends DialogFragment {
 
     private static final String ARG_TITLE = "arg_title";
     private static final String ARG_DETAIL = "arg_detail";
@@ -41,9 +39,9 @@ public class EditDeleteDoneFragment extends Fragment {
     private TextView mTextViewDate;
     private TextView mTextHour;
 
-    private ImageButton mImageButtonDone;
-    private ImageButton mImageButtonDelete;
-    private ImageButton mImageButtonEdit;
+    private Button mButtonDone;
+    private Button mButtonDelete;
+    private Button mButtonEdit;
 
 //    private String title;
 //    private String detail;
@@ -52,6 +50,14 @@ public class EditDeleteDoneFragment extends Fragment {
 
     public EditDeleteDoneFragment() {
         // Required empty public constructor
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // set Dialog Fragment Size and Title
+        getDialog().getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        getDialog().setTitle("اطلاعات برنامه");
     }
 
     @Override
@@ -92,9 +98,9 @@ public class EditDeleteDoneFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_edit_delete_done, container, false);
         mTextViewTitle     = view.findViewById(R.id.edittext_edit_done_remove_title);
         mTextViewDetail    = view.findViewById(R.id.edittext_edit_done_remove_detail);
-        mImageButtonDone   = view.findViewById(R.id.imagebutton_done);
-        mImageButtonDelete = view.findViewById(R.id.imagebutton_delete);
-        mImageButtonEdit   = view.findViewById(R.id.imagebutton_edit);
+        mButtonDone   = view.findViewById(R.id.button_done);
+        mButtonDelete = view.findViewById(R.id.button_delete);
+        mButtonEdit   = view.findViewById(R.id.button_edit);
 
 //        mEditTextTitle.setText(title);
 //        mEditTextDetail.setText(detail);
@@ -108,7 +114,7 @@ public class EditDeleteDoneFragment extends Fragment {
         final MyDialogFragment myDialogFragment = new MyDialogFragment();
 
 
-        mImageButtonDelete.setOnClickListener(new View.OnClickListener() {
+        mButtonDelete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 //                myDialogFragment.newInstance(title, detail).show(fragmentManager, "Custom Dialog Manager");
@@ -117,23 +123,31 @@ public class EditDeleteDoneFragment extends Fragment {
         });
 
 
-        mImageButtonEdit.setOnClickListener(new View.OnClickListener() {
+        mButtonEdit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Fragment:
+//                FragmentManager fragmentManager = getFragmentManager();
+//                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//                EditFragment editFragment = EditFragment.newInstance(work_id);
+////                AddItemToListFragment editFragment = new AddItemToListFragment();
+//                fragmentTransaction.replace(R.id.container_edit_delete_done, editFragment);
+//                fragmentTransaction.commit();
+////                editItem();
+
+                // Dialog Fragment:
                 FragmentManager fragmentManager = getFragmentManager();
-                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
                 EditFragment editFragment = EditFragment.newInstance(work_id);
-//                AddItemToListFragment editFragment = new AddItemToListFragment();
-                fragmentTransaction.replace(R.id.container_edit_delete_done, editFragment);
-                fragmentTransaction.commit();
-//                editItem();
+                editFragment.show(fragmentManager, "Custom Dialog Fragment");
+
             }
         });
 
-        mImageButtonDone.setOnClickListener(new View.OnClickListener() {
+        mButtonDone.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 changeToDone();
+                dismiss();
             }
         });
         return view;
@@ -157,7 +171,6 @@ public class EditDeleteDoneFragment extends Fragment {
         currentWorksModel.setDone(true);
 
         WorksRepository.getInstance(getActivity()).addWorkDone(currentWorksModel);
-        Toast.makeText(getActivity(), "Done Number: " + String.valueOf(WorksRepository.getInstance(getActivity()).getmWorkListDone().size()), Toast.LENGTH_SHORT).show();
 
         mAdapter.notifyItemInserted(0);
 
@@ -177,7 +190,6 @@ public class EditDeleteDoneFragment extends Fragment {
             mAdapterDone.notifyDataSetChanged();
         }
 
-//        Toast.makeText(getActivity(), String.valueOf(worksRepositoryDone.getmWorkListDone().size()), Toast.LENGTH_SHORT).show();
     }//changeToDone
 
     public static EditDeleteDoneFragment newInstance(UUID id){ //@NonNull yani crimeId nemitune Null bashe
